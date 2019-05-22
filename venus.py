87f4e953fe2556c7e069279321607957a711662b
Alex <alexcpu@gmail.com>
2019-05-22 22:07:45 +0100

    Added monitors

--- venus.py
+++ venus.py
@@ -73,10 +73,10 @@ async def write_to_influx(dbhost, dbport, mbmeters, dbname='victron'):
                 # reg_block[23] = Genset L1
                 # reg_block[24] = Genset L2
                 # reg_block[25] = Genset L3
-				# reg_block[26] = Active input source
+                # reg_block[26] = Active input source
 
                 
-				datapoint['tags']['system'] = 1
+                datapoint['tags']['system'] = 1
 
                 # AC Consumption
                 logger.debug(f'Block17: {str(reg_block[17])}')
@@ -87,70 +87,33 @@ async def write_to_influx(dbhost, dbport, mbmeters, dbname='victron'):
                 datapoint['fields']['AC Consumption L2'] = trunc_float(reg_block[18] * scalefactor)
                 datapoint['fields']['AC Consumption L3'] = trunc_float(reg_block[19] * scalefactor)
 
-                # # AC Voltage
-                # logger.debug(f'Block13: {str(reg_block[13])}')
-                # logger.debug(f'AC Voltage SF: {str(np.int16(reg_block[13]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[13]))
-                # logger.debug(f'AC Voltage mult: {str(scalefactor)}')
-                # if reg_block[7]<65535:
-                    # datapoint['fields']['AC Voltage phase A-B'] = trunc_float(reg_block[7] * scalefactor)
-                # if reg_block[8]<65535:
-                    # datapoint['fields']['AC Voltage phase B-C'] = trunc_float(reg_block[8] * scalefactor)
-                # if reg_block[9]<65535:
-                    # datapoint['fields']['AC Voltage phase C-A'] = trunc_float(reg_block[9] * scalefactor)
-                # if reg_block[10]<65535:
-                    # datapoint['fields']['AC Voltage phase A-N'] = trunc_float(reg_block[10] * scalefactor)
-                # if reg_block[11]<65535:
-                    # datapoint['fields']['AC Voltage phase B-N'] = trunc_float(reg_block[11] * scalefactor)
-                # if reg_block[12]<65535:
-                    # datapoint['fields']['AC Voltage phase C-N'] = trunc_float(reg_block[12] * scalefactor)
+                # Grid
+                logger.debug(f'Block17: {str(reg_block[20])}')
+                logger.debug(f'Block18: {str(reg_block[21])}')
+                logger.debug(f'Block19: {str(reg_block[22])}')
+                scalefactor = 1
+                datapoint['fields']['Grid L1'] = trunc_float(np.int16(reg_block[20]) * scalefactor)
+                datapoint['fields']['Grid L2'] = trunc_float(np.int16(reg_block[21]) * scalefactor)
+                datapoint['fields']['Grid L3'] = trunc_float(np.int16(reg_block[22]) * scalefactor)
 
-                # # AC Frequency
-                # logger.debug(f'AC Frequency SF: {str(np.int16(reg_block[17]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[17]))
-                # if reg_block[16]<65535:
-                    # datapoint['fields']['AC Frequency'] = trunc_float(reg_block[16] * scalefactor)
+                # PV On Input
+                logger.debug(f'Block11: {str(reg_block[11])}')
+                logger.debug(f'Block12: {str(reg_block[12])}')
+                logger.debug(f'Block13: {str(reg_block[13])}')
+                scalefactor = 1
+                datapoint['fields']['PV - AC-coupled on input L1'] = trunc_float(reg_block[11] * scalefactor)
+                datapoint['fields']['PV - AC-coupled on input L2'] = trunc_float(reg_block[12] * scalefactor)
+                datapoint['fields']['PV - AC-coupled on input L3'] = trunc_float(reg_block[13] * scalefactor)
 
-                    
-                # # AC Power
-                # logger.debug(f'Block15: {str(reg_block[15])}')
-                # logger.debug(f'AC Power SF: {str(np.int16(reg_block[15]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[15]))
-                # logger.debug(f'AC Power mult: {str(scalefactor)}')
-                # if reg_block[14]<65535:
-                    # datapoint['fields']['AC Power output'] = trunc_float(reg_block[14] * scalefactor)
+                # PV On Output
+                logger.debug(f'Block8: {str(reg_block[8])}')
+                logger.debug(f'Block9: {str(reg_block[9])}')
+                logger.debug(f'Block10: {str(reg_block[10])}')
+                scalefactor = 1
+                datapoint['fields']['PV - AC-coupled on output L1'] = trunc_float(reg_block[8] * scalefactor)
+                datapoint['fields']['PV - AC-coupled on output L2'] = trunc_float(reg_block[9] * scalefactor)
+                datapoint['fields']['PV - AC-coupled on output L3'] = trunc_float(reg_block[10] * scalefactor)
 
-                # # DC Current
-                # logger.debug(f'Block28: {str(reg_block[28])}')
-                # logger.debug(f'DC Current SF: {str(np.int16(reg_block[28]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[28]))
-                # logger.debug(f'DC Current mult: {str(scalefactor)}')
-                # if reg_block[27]<65535:
-                    # datapoint['fields']['DC Current'] = trunc_float(reg_block[27] * scalefactor)
-
-                # # DC Voltage
-                # logger.debug(f'Block30: {str(reg_block[30])}')
-                # logger.debug(f'DC voltage SF: {str(np.int16(reg_block[30]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[30]))
-                # logger.debug(f'DC Voltage mult: {str(scalefactor)}')
-                # if reg_block[29]<65535:
-                    # datapoint['fields']['DC Voltage'] = trunc_float(reg_block[29] * scalefactor)
-
-                # # DC Power
-                # logger.debug(f'Block32: {str(reg_block[32])}')
-                # logger.debug(f'DC Power SF: {str(np.int16(reg_block[32]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[32]))
-                # logger.debug(f'DC Power mult: {str(scalefactor)}')
-                # if reg_block[31]<65535:
-                    # datapoint['fields']['DC Power input'] = trunc_float(reg_block[31] * scalefactor)
-
-                # # Inverter Temp 
-                # logger.debug(f'Block37: {str(reg_block[37])}')
-                # logger.debug(f'Temp SF: {str(np.int16(reg_block[37]))}')
-                # scalefactor = np.float_power(10,np.int16(reg_block[37]))
-                # logger.debug(f'Temp mult: {str(scalefactor)}')
-                # if reg_block[34]<65535:
-                    # datapoint['fields']['Inverter Temperature'] = trunc_float(reg_block[34] * scalefactor)
 
                 datapoint['time'] = str(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat())
                 logger.debug(f'Writing to Influx: {str(datapoint)}')
@@ -165,141 +128,59 @@ async def write_to_influx(dbhost, dbport, mbmeters, dbname='victron'):
                     logger.error('Send or receive error!')
                 elif client.last_error() == 5:
                     logger.error('Timeout during send or receive operation!')
-                    
-            # for x in range(1, mbmeters+1):
-                # # Now loop through this for each meter that is attached.
-                # logger.debug(f'Meter={str(x)}')
-                # reg_block = {}
+
+            reg_block = {}
+            reg_block = client.read_holding_registers(40839, 7)
+            if reg_block:
+                datapoint = {
+                    'measurement': 'Victron',
+                    'tags': {},
+                    'fields': {}
+                }                    
+                # print(reg_block)
+                # reg_block[0] = Battery Voltage (System)
+                # reg_block[1] = Battery Current (System)
+                # reg_block[2] = Battery Power (System)
+                # reg_block[3] = Battery State of Charge (System)
+                # reg_block[4] = Battery state (System)
+                # reg_block[5] = Battery Consumed Amphours (System)
+                # reg_block[6] = Battery Time to Go (System)
+
+                # Battery Voltage
+                logger.debug(f'Block0: {str(reg_block[0])}')
+                scalefactor = 0.1
+                datapoint['fields']['Battery Voltage'] = trunc_float(reg_block[0] * scalefactor)
+
+                # Battery Current
+                logger.debug(f'Block1: {str(reg_block[1])}')
+                scalefactor = 0.1
+                datapoint['fields']['Battery Current'] = trunc_float(np.int16(reg_block[1]) * scalefactor)
+
+                # Battery Power
+                logger.debug(f'Block2: {str(reg_block[2])}')
+                scalefactor = 1
+                datapoint['fields']['Battery Current'] = trunc_float(np.int16(reg_block[2]) * scalefactor)
+
+                # Battery State of Charge
+                logger.debug(f'Block3: {str(reg_block[3])}')
+                scalefactor = 1
+                datapoint['fields']['Battery Current'] = trunc_float(reg_block[3] * scalefactor)
+
                 
-                # # Clear data from inverter, otherwise we publish that again!
-                # datapoint = {
-                    # 'measurement': 'Victron',
-                    # 'tags': {
-                        # 'meter': x
-                    # },
-                    # 'fields': {}
-                # }
+                datapoint['time'] = str(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat())
+                logger.debug(f'Writing to Influx: {str(datapoint)}')
 
-                # # Start point is different for each meter
-                # if x==1:
-                    # reg_block = client.read_holding_registers(40190, 36)
-                # if x==2:
-                    # reg_block = client.read_holding_registers(40364, 36)
-                # if x==3:
-                    # reg_block = client.read_holding_registers(40539, 36)
-                # if reg_block:
-                    # # print(reg_block)
-                    # # reg_block[0] = AC Total current value
-                    # # reg_block[1] = AC Phase A current value
-                    # # reg_block[2] = AC Phase B current value
-                    # # reg_block[3] = AC Phase C current value
-                    # # reg_block[4] = AC current scale factor
-                    # # reg_block[5] = AC Phase Line (average) to N voltage value
-                    # # reg_block[6] = AC Phase A to N voltage value
-                    # # reg_block[7] = AC Phase B to N voltage value
-                    # # reg_block[8] = AC Phase C to N voltage value
-                    # # reg_block[9] = AC Phase Line to Line voltage value
-                    # # reg_block[10] = AC Phase A to B voltage value
-                    # # reg_block[11] = AC Phase B to C voltage value
-                    # # reg_block[12] = AC Phase C to A voltage value
-                    # # reg_block[13] = AC voltage scale factor
-                    # # reg_block[14] = AC Frequency value
-                    # # reg_block[15] = AC Frequency scale factor
-                    # # reg_block[16] = Total Real Power
-                    # # reg_block[17] = Phase A Real Power
-                    # # reg_block[18] = Phase B Real Power
-                    # # reg_block[19] = Phase C Real Power
-                    # # reg_block[20] = Real Power scale factor
-                    # # reg_block[21] = Total Apparent Power
-                    # # reg_block[22] = Phase A Apparent Power
-                    # # reg_block[23] = Phase B Apparent Power
-                    # # reg_block[24] = Phase C Apparent Power
-                    # # reg_block[25] = Apparent Power scale factor
-                    # # reg_block[26] = Total Reactive Power
-                    # # reg_block[27] = Phase A Reactive Power
-                    # # reg_block[28] = Phase B Reactive Power
-                    # # reg_block[29] = Phase C Reactive Power
-                    # # reg_block[30] = Reactive Power scale factor
-                    # # reg_block[31] = Average Power Factor
-                    # # reg_block[32] = Phase A Power Factor
-                    # # reg_block[33] = Phase B Power Factor
-                    # # reg_block[34] = Phase C Power Factor
-                    # # reg_block[35] = Power Factor scale factor
-                    
-                    # logger.debug(f'meter reg_block: {str(reg_block)}')
-
-                    # # AC Current
-                    # logger.debug(f'AC Current SF: {str(np.int16(reg_block[4]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[4]))
-                    # datapoint['fields']['AC Total Current'] = trunc_float(np.int16(reg_block[0]) * scalefactor)
-                    # datapoint['fields']['AC Current phase A'] = trunc_float(np.int16(reg_block[1]) * scalefactor)
-                    # datapoint['fields']['AC Current phase B'] = trunc_float(np.int16(reg_block[2]) * scalefactor)
-                    # datapoint['fields']['AC Current phase C'] = trunc_float(np.int16(reg_block[3]) * scalefactor)
-
-                    # # AC Voltage
-                    # logger.debug(f'AC Voltage SF: {str(np.int16(reg_block[13]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[13]))
-                    # datapoint['fields']['AC Voltage phase L-N'] = trunc_float(np.int16(reg_block[5]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase A-N'] = trunc_float(np.int16(reg_block[6]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase B-N'] = trunc_float(np.int16(reg_block[7]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase C-N'] = trunc_float(np.int16(reg_block[8]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase L-L'] = trunc_float(np.int16(reg_block[9]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase A-B'] = trunc_float(np.int16(reg_block[10]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase B-C'] = trunc_float(np.int16(reg_block[11]) * scalefactor)
-                    # datapoint['fields']['AC Voltage phase C-A'] = trunc_float(np.int16(reg_block[12]) * scalefactor)
-
-                    # # AC Frequency
-                    # logger.debug(f'AC Frequency SF: {str(np.int16(reg_block[15]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[15]))
-                    # datapoint['fields']['AC Frequency'] = trunc_float(np.int16(reg_block[14]) * scalefactor)
-                    
-                    # # AC Real Power
-                    # logger.debug(f'AC Real Power SF: {str(np.int16(reg_block[20]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[20]))
-                    # datapoint['fields']['AC Total Real Power'] = trunc_float(np.int16(reg_block[16]) * scalefactor)
-                    # datapoint['fields']['AC Real Power Phase A'] = trunc_float(np.int16(reg_block[17]) * scalefactor)
-                    # datapoint['fields']['AC Real Power Phase B'] = trunc_float(np.int16(reg_block[18]) * scalefactor)
-                    # datapoint['fields']['AC Real Power Phase C'] = trunc_float(np.int16(reg_block[19]) * scalefactor)
-                    
-                    # # AC Apparent Power
-                    # logger.debug(f'AC Apparent Power SF: {str(np.int16(reg_block[25]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[25]))
-                    # datapoint['fields']['AC Total Apparent Power'] = trunc_float(np.int16(reg_block[21]) * scalefactor)
-                    # datapoint['fields']['AC Apparent Power Phase A'] = trunc_float(np.int16(reg_block[22]) * scalefactor)
-                    # datapoint['fields']['AC Apparent Power Phase B'] = trunc_float(np.int16(reg_block[23]) * scalefactor)
-                    # datapoint['fields']['AC Apparent Power Phase C'] = trunc_float(np.int16(reg_block[24]) * scalefactor)
-
-                    # # AC Reactive Power
-                    # logger.debug(f'AC Reactive Power SF: {str(np.int16(reg_block[30]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[30]))
-                    # datapoint['fields']['AC Total Reactive Power'] = trunc_float(np.int16(reg_block[26]) * scalefactor)
-                    # datapoint['fields']['AC Reactive Power Phase A'] = trunc_float(np.int16(reg_block[27]) * scalefactor)
-                    # datapoint['fields']['AC Reactive Power Phase B'] = trunc_float(np.int16(reg_block[28]) * scalefactor)
-                    # datapoint['fields']['AC Reactive Power Phase C'] = trunc_float(np.int16(reg_block[29]) * scalefactor)
-
-                    # # AC Power Factor
-                    # logger.debug(f'AC Power Factor SF: {str(np.int16(reg_block[30]))}')
-                    # scalefactor = np.float_power(10,np.int16(reg_block[35]))
-                    # datapoint['fields']['AC Average Power Factor'] = trunc_float(np.int16(reg_block[31]) * scalefactor)
-                    # datapoint['fields']['AC Power Factor Phase A'] = trunc_float(np.int16(reg_block[32]) * scalefactor)
-                    # datapoint['fields']['AC Power Factor Phase B'] = trunc_float(np.int16(reg_block[33]) * scalefactor)
-                    # datapoint['fields']['AC Power Factor Phase C'] = trunc_float(np.int16(reg_block[34]) * scalefactor)
-
-                    
-                    # datapoint['time'] = str(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat())
-                    # logger.debug(f'Writing to Influx: {str(datapoint)}')
-
-                    # await solar_client.write(datapoint)
+                await solar_client.write(datapoint)
 
 
-                # else:
-                    # # Error during data receive
-                    # if client.last_error() == 2:
-                        # logger.error(f'Failed to connect to Victron Host {client.host()}!')
-                    # elif client.last_error() == 3 or client.last_error() == 4:
-                        # logger.error('Send or receive error!')
-                    # elif client.last_error() == 5:
-                        # logger.error('Timeout during send or receive operation!')               
+            else:
+                # Error during data receive
+                if client.last_error() == 2:
+                    logger.error(f'Failed to connect to Victron Host {client.host()}!')
+                elif client.last_error() == 3 or client.last_error() == 4:
+                    logger.error('Send or receive error!')
+                elif client.last_error() == 5:
+                    logger.error('Timeout during send or receive operation!')               
                 
                 
                 
